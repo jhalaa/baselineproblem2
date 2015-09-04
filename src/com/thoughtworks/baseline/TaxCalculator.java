7c9d56643e2967df7e869724886be82ad0e08259
jhalaa <jhalaachinoy@gmail.com>
2015-09-04 16:14:54 +0530

    the basic sales tax of an object should be zero

--- src/com/thoughtworks/baseline/TaxCalculator.java
+++ src/com/thoughtworks/baseline/TaxCalculator.java
@@ -1,4 +1,7 @@
 package com.thoughtworks.baseline;
 
 public class TaxCalculator {
+    public double basicSalesTax() {
+        return 10.0;
+    }
 }
--- test/com/thoughtworks/baseline/TaxCalculatorTest.java
+++ test/com/thoughtworks/baseline/TaxCalculatorTest.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.baseline;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class TaxCalculatorTest {
+
+    @Test
+    public void basicSalesTaxIsTenPercent()
+    {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        assertEquals(10.0,taxCalculator.basicSalesTax(),0.01);
+    }
+
 }
